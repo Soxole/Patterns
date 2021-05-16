f54d046f42a39579e78ee421b617dedfa4918066
vstudio2018 <ivanovevgeny@mail.ru>
2021-05-16 21:36:06 +0300

    add fix

--- Composite.h
+++ Composite.h
@@ -3,97 +3,100 @@
 #include <list>
 #include <string>
 
-using namespace std;
+
 //TODO: pattern composite; incorrect working 
 
 /*
-Применимость: Паттерн Компоновщик встречается в любых задачах, которые связаны с построением дерева.
-Самый простой пример — составные элементы GUI, которые тоже можно рассматривать как дерево.
-Признаки применения паттерна: Если из объектов строится древовидная структура, и со всеми объектами дерева,
-как и с самим деревом работают через общий интерфейс.
+ РљРѕРјРїРѕРЅРѕРІС‰РёРє вЂ” СЌС‚Рѕ СЃС‚СЂСѓРєС‚СѓСЂРЅС‹Р№ РїР°С‚С‚РµСЂРЅ РїСЂРѕРµРєС‚РёСЂРѕРІР°РЅРёСЏ, РєРѕС‚РѕСЂС‹Р№ РїРѕР·РІРѕР»СЏРµС‚ СЃРіСЂСѓРїРїРёСЂРѕРІР°С‚СЊ РјРЅРѕР¶РµСЃС‚РІРѕ РѕР±СЉРµРєС‚РѕРІ РІ РґСЂРµРІРѕРІРёРґРЅСѓСЋ СЃС‚СЂСѓРєС‚СѓСЂСѓ,
+Р° Р·Р°С‚РµРј СЂР°Р±РѕС‚Р°С‚СЊ СЃ РЅРµР№ С‚Р°Рє, РєР°Рє Р±СѓРґС‚Рѕ СЌС‚Рѕ РµРґРёРЅРёС‡РЅС‹Р№ РѕР±СЉРµРєС‚.
+ РџСЂРёРјРµРЅРёРјРѕСЃС‚СЊ: РџР°С‚С‚РµСЂРЅ РљРѕРјРїРѕРЅРѕРІС‰РёРє РІСЃС‚СЂРµС‡Р°РµС‚СЃСЏ РІ Р»СЋР±С‹С… Р·Р°РґР°С‡Р°С…, РєРѕС‚РѕСЂС‹Рµ СЃРІСЏР·Р°РЅС‹ СЃ РїРѕСЃС‚СЂРѕРµРЅРёРµРј РґРµСЂРµРІР°.
+РЎР°РјС‹Р№ РїСЂРѕСЃС‚РѕР№ РїСЂРёРјРµСЂ вЂ” СЃРѕСЃС‚Р°РІРЅС‹Рµ СЌР»РµРјРµРЅС‚С‹ GUI, РєРѕС‚РѕСЂС‹Рµ С‚РѕР¶Рµ РјРѕР¶РЅРѕ СЂР°СЃСЃРјР°С‚СЂРёРІР°С‚СЊ РєР°Рє РґРµСЂРµРІРѕ.
+ РџСЂРёР·РЅР°РєРё РїСЂРёРјРµРЅРµРЅРёСЏ РїР°С‚С‚РµСЂРЅР°: Р•СЃР»Рё РёР· РѕР±СЉРµРєС‚РѕРІ СЃС‚СЂРѕРёС‚СЃСЏ РґСЂРµРІРѕРІРёРґРЅР°СЏ СЃС‚СЂСѓРєС‚СѓСЂР°, Рё СЃРѕ РІСЃРµРјРё РѕР±СЉРµРєС‚Р°РјРё РґРµСЂРµРІР°,
+РєР°Рє Рё СЃ СЃР°РјРёРј РґРµСЂРµРІРѕРј СЂР°Р±РѕС‚Р°СЋС‚ С‡РµСЂРµР· РѕР±С‰РёР№ РёРЅС‚РµСЂС„РµР№СЃ.
 */
 
+
+//Abstract Component; stores a pointer yourself
 class IComponent
 {
 public:
-	//IComponent() = default;
+	IComponent() = default;
 	virtual ~IComponent() = default;
 
 	//setter
-	void setParent(shared_ptr<IComponent> parent)
+	void setParent(const std::shared_ptr<IComponent> &parent)
 	{
 		m_parent_ = parent;
 	}
 	//getter; unused
-	[[nodiscard]] shared_ptr<IComponent> getParent() const
+	[[nodiscard]] std::shared_ptr<IComponent> getParent() const
 	{
-		return make_shared<IComponent>();
+		auto tmp = m_parent_.lock();
+		if (tmp)
+			return tmp;
+
+		return nullptr;
 	}
-	
+
 	//empty methods 
-	[[maybe_unused]] virtual void addComp(shared_ptr<IComponent> component)
+	[[maybe_unused]] virtual void addComp(std::shared_ptr<IComponent> &&component)
 	{}
-	[[maybe_unused]] virtual void removeComp(shared_ptr<IComponent> component)
+	[[maybe_unused]] virtual void removeComp(const std::shared_ptr<IComponent> &component)
 	{}
 
 	[[nodiscard]] virtual bool isComposite() const {
 		return false;
 	}
-	//any operation return string
-	[[nodiscard]] virtual string operation() const {
+	//some operation return string
+	[[nodiscard]] virtual std::string operation() const {
 		return "";
 	}
 
 protected:
-	weak_ptr<IComponent> m_parent_; //weak_ptr
+	std::weak_ptr<IComponent> m_parent_; //weak_ptr
 };
-/*
- * Класс Лист представляет собой конечные объекты структуры. Лист не может иметь
- * вложенных компонентов.
- *
- * Обычно объекты Листьев выполняют фактическую работу, тогда как объекты
- * Контейнера лишь делегируют работу своим подкомпонентам.
- */
 
+//Leaf
 class Leaf : public IComponent
 {
 public:
-	[[nodiscard]] string operation() const override
+	[[nodiscard]] std::string operation() const override
 	{
 		return "leaf";
 	}
 };
 
-/**
- * Класс Контейнер содержит сложные компоненты, которые могут иметь вложенные
- * компоненты. Обычно объекты Контейнеры делегируют фактическую работу своим
- * детям, а затем «суммируют» результат.
- */
-
+//Composite derived from IComponent; stores pointer to IComponent
 class Composite final : public IComponent, public std::enable_shared_from_this<Composite>
 {
 public:
-	void addComp(shared_ptr<IComponent> component) override
+	void addComp(std::shared_ptr<IComponent> &&component) override
 	{
+		std::weak_ptr tmp_this_composite(shared_from_this());
 		m_children_.emplace_back(component);
-		component->setParent(shared_from_this());	//this
+		if (!tmp_this_composite.expired())
+		{
+			component->setParent(tmp_this_composite.lock());	//ptr to this
+		}
 	}
 
-	void removeComp(shared_ptr<IComponent> component) override
+	void removeComp(const std::shared_ptr<IComponent> &component) override
 	{
 		m_children_.remove(component);
 		component->setParent(nullptr);
 	}
 
-	[[nodiscard]] string operation() const override
+	[[nodiscard]] std::string operation() const override
 	{
-		string result;
+		std::string result;
 
-		for (const auto &component : m_children_) {
+		for (const auto &component : m_children_)
+		{
 			if (component == m_children_.back())
 			{
 				result += component->operation();
-			}else{
+			}
+			else {
 				result += component->operation() + " + ";
 			}
 		}
@@ -101,23 +104,19 @@ public:
 	}
 
 protected:
-	list<shared_ptr<IComponent>> m_children_;
+	std::list<std::shared_ptr<IComponent>> m_children_;
 };
 
 /*
 
- * Благодаря тому, что операции управления потомками объявлены в базовом классе
- * Компонента, клиентский код может работать как с простыми, так и со сложными
- * компонентами, вне зависимости от их конкретных классов.
 
-
-void client_code(const unique_ptr<IComponent> &component) {
+void client_code(const shared_ptr<IComponent> &component) {
 
 	std::cout << "RESULT: " << component->operation();
 }
 
 
-void client_code2(unique_ptr<IComponent> &component1,unique_ptr<IComponent> &component2) {
+void client_code2(shared_ptr<IComponent> &component1, shared_ptr<IComponent> &component2) {
 
 	if (component1->isComposite()) {
 		component1->addComp(move(component2));
@@ -127,30 +126,30 @@ void client_code2(unique_ptr<IComponent> &component1,unique_ptr<IComponent> &com
 
 int main()
 {
-	unique_ptr<IComponent> simple{ make_unique<Leaf>() };
+	shared_ptr<IComponent> tree{ make_shared<Composite>() };
+	shared_ptr<IComponent> simple{ make_shared<Leaf>() };
 	std::cout << "Client: I've got a simple component:\n";
 	client_code(simple);
 	cout << "\n";
 
-	unique_ptr<IComponent> tree{ make_unique<Composite>() };
-	unique_ptr<IComponent> branch{ make_unique<Composite>() };
+	shared_ptr<IComponent> branch{ make_shared<Composite>() };
+	shared_ptr<IComponent> leaf_1{ make_shared<Leaf>() };
+	shared_ptr<IComponent> leaf_2{ make_shared<Leaf>() };
+	shared_ptr<IComponent> leaf_3{ make_shared<Leaf>() };
 
-	unique_ptr<IComponent> leaf_1{ make_unique<Leaf>() };
-	unique_ptr<IComponent> leaf_2{ make_unique<Leaf>() };
-	unique_ptr<IComponent> leaf_3{ make_unique<Leaf>() };
-
-	branch->addComp(move(leaf_1));
-	branch->addComp(move(leaf_2));
-	unique_ptr<IComponent> branch2{ make_unique<Composite>() };
-	branch2->addComp(move(leaf_3));
-	tree->addComp(move(branch));
-	tree->addComp(move(branch2));
+	branch->addComp(leaf_1);
+	branch->addComp(leaf_2);
+	shared_ptr<IComponent> branch2{ make_shared<Composite>() };
+	branch2->addComp(leaf_3);
+	tree->addComp(branch);
+	tree->addComp(branch2);
 
 	std::cout << "Client: Now I've got a composite tree:\n";
 	client_code(tree);
 	std::cout << "\n\n";
 
 	std::cout << "Client: I don't need to check the components classes even when managing the tree:\n";
+
 	client_code2(tree, simple);
 	std::cout << "\n";
 }

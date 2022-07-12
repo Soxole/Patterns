b17a105f67e20619698049f433c3212fc59d4dd5
vstudio2018 <ivanovevgeny@mail.ru>
2022-07-12 19:05:16 +0300

    fix Chain of Responsibility

--- cor_01.h
+++ cor_01.h
@@ -1,67 +1,67 @@
 #pragma once
 #include <memory>
-
-using std::shared_ptr;
+#include <string>
 
 /*
-Цепочка обязанностей — это поведенческий паттерн проектирования, который позволяет передавать
-запросы последовательно по цепочке обработчиков. Каждый последующий обработчик решает,
-может ли он обработать запрос сам и стоит ли передавать запрос дальше по цепи.
+Р¦РµРїРѕС‡РєР° РѕР±СЏР·Р°РЅРЅРѕСЃС‚РµР№ вЂ” СЌС‚Рѕ РїРѕРІРµРґРµРЅС‡РµСЃРєРёР№ РїР°С‚С‚РµСЂРЅ, РїРѕР·РІРѕР»СЏСЋС‰РёР№ РїРµСЂРµРґР°РІР°С‚СЊ Р·Р°РїСЂРѕСЃ
+РїРѕ С†РµРїРѕС‡РєРµ РїРѕС‚РµРЅС†РёР°Р»СЊРЅС‹С… РѕР±СЂР°Р±РѕС‚С‡РёРєРѕРІ, РїРѕРєР° РѕРґРёРЅ РёР· РЅРёС… РЅРµ РѕР±СЂР°Р±РѕС‚Р°РµС‚ Р·Р°РїСЂРѕСЃ.
 */
 
 class IHandler
 {
 public:
+	IHandler() = default;
+	IHandler(const IHandler &) = default;
+	IHandler &operator=(const IHandler &) = default;
+	IHandler(IHandler &&) noexcept = default;
+	IHandler &operator=(IHandler &&) noexcept = default;
 	virtual ~IHandler() = default;
-	virtual shared_ptr<IHandler> set_next(shared_ptr<IHandler> handler) = 0;
+
+	virtual std::shared_ptr<IHandler> set_next(std::shared_ptr<IHandler> handler) = 0;
 	virtual std::string handle(std::string request) = 0;
 };
 
 class AbstractHandler : public IHandler
 {
 public:
-	AbstractHandler() : m_next_handler(nullptr)
-	{
-	}
-	shared_ptr<IHandler> set_next(shared_ptr<IHandler> handler) override {
-		m_next_handler = handler;
+	AbstractHandler() : m_next_handler(nullptr) { }
 
+	std::shared_ptr<IHandler> set_next(std::shared_ptr<IHandler> handler) override {
+		m_next_handler = handler;
 		return m_next_handler;
 	}
 
 	[[nodiscard]] std::string handle(std::string request) override {
 		if (m_next_handler)
 			return m_next_handler->handle(request);
-
 		return "";
 	}
-
 private:
-	shared_ptr<IHandler> m_next_handler;
+	std::shared_ptr<IHandler> m_next_handler;
 };
+
 //concrete handler #1
-class MonkeyHandler : public AbstractHandler
+class MonkeyHandler final : public AbstractHandler
 {
 public:
-
 	[[nodiscard]] std::string handle(std::string request) override {
 		if (request == "Banana")
 			return "Monkey: I'll eat the " + request + ".\n";
-
 		return AbstractHandler::handle(request);
 	}
 };
+
 //concrete handler #2
-class SquirreHandler : public AbstractHandler
+class SquirrelHandler final : public AbstractHandler
 {
 public:
 	[[nodiscard]] std::string handle(std::string request) override {
 		if (request == "Nut")
 			return "Squirrel: I'll eat the " + request + ".\n";
-
 		return AbstractHandler::handle(request);
 	}
 };
+
 //concrete handler #3
 class DogHandler final : public AbstractHandler
 {
@@ -69,39 +69,38 @@ public:
 	[[nodiscard]] std::string handle(std::string request) override {
 		if (request == "MeatBall")
 			return "Dog: I'll eat the " + request + ".\n";
-
 		return AbstractHandler::handle(request);
 	}
 };
 
-/*
-void clientCode(IHandler &handler)
-{
-	vector food{ "Nut", "Banana", "Cup of coffee" };
+//main()
+#if 0
+
+void clientCode(IHandler &handler) {
+	std::vector food{ "Nut", "Banana", "Cup of coffee" };
 	for (const auto &it : food) {
-		cout << "Client: Who wants a " << it << "?\n";
-		const string res = handler.handle(it);
-		if (!res.empty())
-		{
-			cout << " " << res;
+		std::cout << "Client: Who wants a " << it << "?\n";
+		const std::string res = handler.handle(it);
+		if (!res.empty()) {
+			std::cout << " " << res;
 		}
 		else
-			cout << " " << it << " was left untouched.\n";
+			std::cout << " " << it << " was left untouched.\n";
 	}
 }
 
 int main()
 {
-	auto monkey = make_unique<MonkeyHandler>();
-	auto squirrel = make_shared<SquirreHandler>();
-	auto dog = make_unique<DogHandler>();
-	monkey->setNext(squirrel);
+	auto monkey = std::make_unique<MonkeyHandler>();
+	auto squirrel = std::make_shared<SquirrelHandler>();
+	auto dog = std::make_unique<DogHandler>();
+	monkey->set_next(squirrel);
 
-	cout << "Chain: Monkey > Squirrel > Dog\n\n";
+	std::cout << "Chain: Monkey > Squirrel > Dog\n\n";
 	clientCode(*monkey);
-	cout << "\n";
-	cout << "Subchain: Squirrel > Dog\n\n";
+	std::cout << "\n";
+	std::cout << "Subchain: Squirrel > Dog\n\n";
 	clientCode(*squirrel);
 }
 
-*/
+#endif

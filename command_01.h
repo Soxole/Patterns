2354c3ac57f6de2fd658e7932d6de7e62f677912
vstudio2018 <ivanovevgeny@mail.ru>
2022-07-20 20:40:32 +0300

    fix command

--- command_01.h
+++ command_01.h
@@ -1,67 +1,68 @@
-// ReSharper disable CppInconsistentNaming
 #pragma once
 #include <iostream>
 #include <string>
 
 /*
-	������� � ��� ������������� �������, ����������� ������������ ������� ��� ������� �������� � ��������� �������.
-	����������� ������� Command ����:
-		������� ����������� ���������. ��� ��������� ������ ������� (�������) ���������� ��������� ������������ ������������������ ��������.
-		���������� ����������������� ������� ����������� ���������, ������� ������� � ������� ��� ������������ �������� ������ (undo) �
-		������� (redo) ��������.
-		����� ��������-��������������� ������ ������� ��������� ������ � ����������� ����������������.
-
+Команда — это поведенческий паттерн, позволяющий заворачивать запросы или
+простые операции в отдельные объекты.
+Это позволяет откладывать выполнение команд, выстраивать их в очереди,
+а также хранить историю и делать отмену.
  */
 
 
 
-//interface
-class Command_i
+ //interface
+class ICommand
 {
 public:
-	virtual ~Command_i() = default;
+	ICommand() = default;
+	ICommand(ICommand const &) = default;
+	ICommand &operator=(ICommand const &) = default;
+	ICommand(ICommand &&) noexcept = default;
+	ICommand &operator=(ICommand &&) noexcept = default;
+	virtual ~ICommand() = default;
+
 	virtual void execute() const = 0;
 };
 
 //a simple command
-class SimpleCommand : public Command_i
+class SimpleCommand final : public ICommand
 {
 public:
-	explicit SimpleCommand(std::string &&pay_load) : m_pay_load(std::forward<std::string>(pay_load))
-	{
-	}
-	void execute() const override
-	{
-		std::cout << "SimpleCommand: See, I can do simple things like printing (" << m_pay_load << ")\n";
-	}
+	explicit SimpleCommand(std::string &&pay_load)
+		: m_pay_load(std::move(pay_load)) {}
 
+	void execute() const override {
+		std::cout << "SimpleCommand: See, I can do simple things like printing ("
+			<< m_pay_load << ")\n";
+	}
 private:
 	std::string m_pay_load;
 };
 
 //receiver: the function with output 
-class Receiver
+class Receiver final
 {
 public:
-	void doSomething(const std::string_view a) const
-	{
+	void doSomething(const std::string_view a) const {
 		std::cout << "Receiver: Working on (" << a << ".)\n";
+	}
+	void doSomethingElse(const std::string_view b) const {
+		std::cout << "Receiver: Also working on (" << b << ".)\n";
 	}
 };
 
-//a big command: 1 pointer to the Receiver and two strings (a, b)
-class ComplexCommand : public Command_i
+class ComplexCommand final : public ICommand
 {
 public:
 	ComplexCommand(std::unique_ptr<Receiver> receiver, std::string &&a, std::string &&b)
-		: m_receiver_(std::move(receiver)), m_a(std::forward<std::string>(a)), m_b(std::forward<std::string>(b))
-	{
-	}
-	void execute() const override
-	{
+		: m_receiver_(std::move(receiver)), m_a(std::move(a)),
+		m_b(std::move(b)) {}
+
+	void execute() const override {
 		std::cout << "ComplexCommand: Complex stuff should be done by a receiver object.\n";
 		m_receiver_->doSomething(m_a);
-		m_receiver_->doSomething(m_b);
+		m_receiver_->doSomethingElse(m_b);
 	}
 private:
 	std::unique_ptr<Receiver> m_receiver_;
@@ -69,49 +70,41 @@ private:
 	std::string m_b;
 };
 
-//invoker: 2 pointers to the class Command_i
-class Invoker
+class Invoker final
 {
 public:
-	~Invoker() = default;
-
-	//the func receives a smart pointer <SimpleCommand> and <ComplexCommand>; <Command_i> = <SimpleCommand> 
-	void set_on_start(std::unique_ptr<Command_i> command)
-	{
+	void set_on_start(std::unique_ptr<ICommand> command) {
 		m_on_start_ = std::move(command);
 	}
-
-	void set_on_finish(std::unique_ptr<Command_i> command)
-	{
+	void set_on_finish(std::unique_ptr<ICommand> command) {
 		m_on_finish_ = std::move(command);
 	}
 
-	void do_something_important() const
-	{
+	void do_something_important() const {
 		std::cout << "Invoker: Does anybody want something done before I begin?\n";
-		if (m_on_start_)
-		{
+		if (m_on_start_) {
 			m_on_start_->execute();
 		}
 		std::cout << "Invoker: ...doing something really important...\n";
 		std::cout << "Invoker: Does anybody want something done after I finish?\n";
-		if (m_on_finish_)
-		{
+		if (m_on_finish_) {
 			m_on_finish_->execute();
 		}
 	}
-
 private:
-	std::unique_ptr<Command_i> m_on_start_;
-	std::unique_ptr<Command_i> m_on_finish_;
+	std::unique_ptr<ICommand> m_on_start_;
+	std::unique_ptr<ICommand> m_on_finish_;
 };
 
-/*
-  	auto invoker_main = make_unique<Invoker>();
-	invoker_main->set_on_start(make_unique<SimpleCommand>("Say Hi"));
-	auto receiver_main = unique_ptr<Receiver>();
-	invoker_main->set_on_finish(make_unique<ComplexCommand>(move(receiver_main), 
-		"Send email", "Save report"));
-	invoker_main->do_something_important();
 
- */
+//main()
+#if 0
+
+const auto invoker_main = std::make_unique<Invoker>();
+invoker_main->set_on_start(std::make_unique<SimpleCommand>("Say Hi"));
+auto receiver_main = std::unique_ptr<Receiver>();
+invoker_main->set_on_finish(std::make_unique<ComplexCommand>(move(receiver_main),
+	"Send email", "Save report"));
+invoker_main->do_something_important();
+
+#endif
--- command_02.h
+++ command_02.h
@@ -2,52 +2,28 @@
 #include <iostream>
 #include <memory>
 
-/*
-Рассмотрим реализацию паттерна Command на примере игры «Шахматы». Имитируем возможность выполнения следующих операций:
-
-	Создать новую игру.
-	Открыть существующую игру.
-	Сохранить игру.
-	Сделать очередной ход.
-	Отменить последний ход.
-	
- */
-
-
-
-using std::cout;
-using std::cin;
-using std::string_view;
-using std::string;
-
-
 class Game
 {
 public:
 	Game() = default;
-	void create() const
-	{
-		cout << "Create game " << "\n";
+	void create() const {
+		std::cout << "Create game " << "\n";
 	}
-	void open(string_view file)
-	{
-		cout << "Open game from -> " << file << "\n";
+	void open(std::string_view file) {
+		std::cout << "Open game from -> " << file << "\n";
 	}
-	void save(string_view file)
-	{
-		cout << "Save game in -> " << file << "\n";
+	void save(std::string_view file) {
+		std::cout << "Save game in -> " << file << "\n";
 	}
-	void make_move(string_view move)
-	{
-		cout << "Make move -> " << move << "\n";
+	void make_move(std::string_view move) {
+		std::cout << "Make move -> " << move << "\n";
 	}
 };
 //inline func opens files
-inline string get_player_input(const string_view promt)
-{
-	string input;
-	cout << promt;
-	cin >> input;
+inline std::string get_player_input(const std::string_view promt) {
+	std::string input;
+	std::cout << promt;
+	std::cin >> input;
 	return input;
 }
 
@@ -55,95 +31,79 @@ inline string get_player_input(const string_view promt)
 class Command
 {
 public:
+	Command() = default;
+	Command(Command const &) = default;
+	Command &operator=(Command const &) = default;
+	Command(Command &&) noexcept = default;
+	Command &operator=(Command &&) noexcept = default;
 	virtual ~Command() = default;
+
 	virtual void execute() = 0;
 protected:
-	explicit Command(std::shared_ptr<Game> ptr) : m_ptr_game_(std::move(ptr))
-	{
-	}
+	explicit Command(std::shared_ptr<Game> ptr) : m_ptr_game_(std::move(ptr)) {}
 	std::shared_ptr<Game> m_ptr_game_;
 };
 //command #1 create game
-class CreateGameCommand : public Command
+class CreateGameCommand final : public Command
 {
 public:
-	explicit CreateGameCommand(std::shared_ptr<Game> ptr) : Command(ptr)
-	{
-	}
-	void execute() override
-	{
+	explicit CreateGameCommand(std::shared_ptr<Game> const ptr) : Command(ptr) {}
+	void execute() override {
 		m_ptr_game_->create();
 	}
 };
 //command #2
-class OpenGameCommand : public Command
+class OpenGameCommand final : public Command
 {
 public:
-	explicit OpenGameCommand(std::shared_ptr<Game> ptr) : Command(std::move(ptr))
-	{
-	}
-	void execute() override
-	{
+	explicit OpenGameCommand(std::shared_ptr<Game> ptr) : Command(std::move(ptr)) {}
+	void execute() override {
 		const auto file_name{ get_player_input("Enter file name: ") };
 		m_ptr_game_->open(file_name);
 	}
 };
 //command #3
-class SaveGameCommand : public Command
+class SaveGameCommand final : public Command
 {
 public:
-	explicit SaveGameCommand(std::shared_ptr<Game> ptr) : Command(std::move(ptr))
-	{
-	}
-	void execute() override
-	{
-		const string file_name{ get_player_input("Enter file name: ") };
+	explicit SaveGameCommand(std::shared_ptr<Game> ptr) : Command(std::move(ptr)) {}
+	void execute() override {
+		const std::string file_name{ get_player_input("Enter file name: ") };
 		m_ptr_game_->save(file_name);
 	}
 };
 //command #4
-class MakeMoveCommand : public Command
+class MakeMoveCommand final : public Command
 {
 public:
-	explicit MakeMoveCommand(std::shared_ptr<Game> ptr) : Command(std::move(ptr))
-	{
-	}
-	void execute() override
-	{
-		// Сохраним игру для возможного последующего отката
+	explicit MakeMoveCommand(std::shared_ptr<Game> ptr) : Command(std::move(ptr)) {}
+	void execute() override {
 		m_ptr_game_->save("TEMP_FILE");
-		const string move{ get_player_input("Enter your move: ") };
+		const std::string move{ get_player_input("Enter your move: ") };
 		m_ptr_game_->make_move(move);
 	}
 };
 //command #5
-class UndoCommand : public Command
+class UndoCommand final : public Command
 {
 public:
-	explicit UndoCommand(std::shared_ptr<Game> ptr) : Command(std::move(ptr))
-	{
-	}
-	void execute() override
-	{
+	explicit UndoCommand(std::shared_ptr<Game> ptr) : Command(std::move(ptr)) {}
+	void execute() override {
 		m_ptr_game_->open("TEMP_FILE");
 	}
 };
 
-/*
-	auto game = make_shared<Game>();
-	vector<unique_ptr<Command> > v;
-	// Создаем новую игру
-	v.emplace_back(make_unique<CreateGameCommand>(game));
-	// Делаем несколько ходов
-	v.emplace_back(make_unique<MakeMoveCommand>(game));
-	v.emplace_back(make_unique<MakeMoveCommand>(game));
-	// Последний ход отменяем
-	v.emplace_back(make_unique<UndoCommand>(game));
-	// Сохраняем игру
-	v.emplace_back(make_unique<SaveGameCommand>(game));
-	for (const auto &it : v)
-	{
-		it->execute();
-	}
+//main()
+#if 0
+auto game = std::make_shared<Game>();
+std::vector<std::unique_ptr<Command> > v;
 
- */
+v.emplace_back(std::make_unique<CreateGameCommand>(game));
+v.emplace_back(std::make_unique<MakeMoveCommand>(game));
+v.emplace_back(std::make_unique<MakeMoveCommand>(game));
+v.emplace_back(std::make_unique<UndoCommand>(game));
+v.emplace_back(std::make_unique<SaveGameCommand>(game));
+for (const auto &it : v) {
+	it->execute();
+}
+#endif
